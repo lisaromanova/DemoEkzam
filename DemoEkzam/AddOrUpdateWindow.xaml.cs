aa081573dfcd57f6534a5dc051f8a5aab7fc2829
Елизавета Романова <soika-peresmeshnica2015@yandex.ru>
2022-10-08 16:35:09 +0300

    Удаление_данных

---
+++ DemoEkzam/AddOrUpdateWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DemoEkzam
+{
+    /// <summary>
+    /// Логика взаимодействия для AddOrUpdateWindow.xaml
+    /// </summary>
+    public partial class AddOrUpdateWindow : Window
+    {
+        public AddOrUpdateWindow()
+        {
+            InitializeComponent();
+            gbId.Visibility = Visibility.Collapsed;
+            nameWindow.Title = "Добавление услуги";
+        }
+        public AddOrUpdateWindow(Service service)
+        {
+            InitializeComponent();
+            gbId.Visibility = Visibility.Visible;
+            nameWindow.Title = "Редактирование услуги";
+        }
+    }
+}
--- DemoEkzam/Pages/Vyvod.xaml.cs
+++ DemoEkzam/Pages/Vyvod.xaml.cs
@@ -34,27 +34,37 @@ namespace DemoEkzam.Pages
 
         private void cbFiltr_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            SearchTitle(list);
+            Filtr(list1);
+            Sort();
+        }
+
+        void Filtr(List<Service> L)
+        {
             switch (cbFiltr.SelectedIndex)
             {
                 case 0:
-                    list1 = list;  
+                    list1 = list;
                     break;
                 case 1:
-                    list1 = list.Where(s=> s.Discount>=0 && s.Discount<0.05).ToList();
+                    list1 = L.Where(s => s.Discount >= 0 && s.Discount < 0.05).ToList();
                     break;
                 case 2:
-                    list1 = list.Where(s => s.Discount >= 0.05 && s.Discount < 0.15).ToList();
+                    list1 = L.Where(s => s.Discount >= 0.05 && s.Discount < 0.15).ToList();
                     break;
                 case 3:
-                    list1 = list.Where(s => s.Discount >= 0.15 && s.Discount < 0.30).ToList();
+                    list1 = L.Where(s => s.Discount >= 0.15 && s.Discount < 0.30).ToList();
                     break;
                 case 4:
-                    list1 = list.Where(s => s.Discount >= 0.30 && s.Discount < 0.70).ToList();
+                    list1 = L.Where(s => s.Discount >= 0.30 && s.Discount < 0.70).ToList();
                     break;
                 case 5:
-                    list1 = list.Where(s => s.Discount >= 0.70 && s.Discount < 1).ToList();
+                    list1 = L.Where(s => s.Discount >= 0.70 && s.Discount < 1).ToList();
                     break;
             }
+        }
+        void Sort()
+        {
             switch (cbSort.SelectedIndex)
             {
                 case 0:
@@ -69,9 +79,59 @@ namespace DemoEkzam.Pages
             tbCountService.Text = index.ToString() + " из " + indexAll.ToString();
         }
 
+        void SearchTitle(List<Service> L)
+        {
+            list1 = L.Where(x => x.Title.Contains(txtNameService.Text)||x.Description.Contains(txtNameService.Text)).ToList();
+        }
+
+        bool CheckZapis(int id)
+        {
+            ClientService lst = DataBase.connection.ClientService.FirstOrDefault(x => x.ServiceID == id);
+            if (lst != null)
+            {
+                MessageBox.Show("Невозможно удалить запись!", "Удаление записи", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
+            Button btnDelete = sender as Button;
+            int id = (int)btnDelete.Tag;
+            if (CheckZapis(id))
+            {
+                Service service = DataBase.connection.Service.FirstOrDefault(x => x.ID == id);
+                DataBase.connection.Service.Remove(service);
+                DataBase.connection.SaveChanges();
+                list = DataBase.connection.Service.ToList();
+                SearchTitle(list);
+                Filtr(list1);
+                Sort();
+                MessageBox.Show("Запись успешно удалена!", "Удаление записи", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
 
+        private void txtNameService_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Filtr(list);
+            SearchTitle(list1);
+            Sort();
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            AddOrUpdateWindow window = new AddOrUpdateWindow();
+            if (window.IsActive)
+            {
+                MessageBox.Show("ervdev");
+            }
+            else
+            {
+                window.Show();
+            }
         }
     }
 }
