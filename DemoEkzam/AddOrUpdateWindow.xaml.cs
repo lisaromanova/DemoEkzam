35f51b038cefe7f28e6226667226390c847538cb
Елизавета Романова <soika-peresmeshnica2015@yandex.ru>
2022-11-21 17:05:44 +0300

    CommitNew

--- DemoEkzam/AddOrUpdateWindow.xaml.cs
+++ DemoEkzam/AddOrUpdateWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using DemoEkzam.Classes;
 
 namespace DemoEkzam
 {
@@ -19,17 +20,48 @@ namespace DemoEkzam
     /// </summary>
     public partial class AddOrUpdateWindow : Window
     {
+        Service service;
         public AddOrUpdateWindow()
         {
             InitializeComponent();
             gbId.Visibility = Visibility.Collapsed;
             nameWindow.Title = "Добавление услуги";
+            tbHeader.Text = "Добавление услуги";
+            btnAddUpdate.Content = "Добавить услугу";
+            gbId.Visibility = Visibility.Collapsed;
         }
         public AddOrUpdateWindow(Service service)
         {
             InitializeComponent();
+            this.service = service;
             gbId.Visibility = Visibility.Visible;
             nameWindow.Title = "Редактирование услуги";
+            tbHeader.Text = "Редактирование услуги";
+            txtId.Text = service.ID.ToString();
+            txtName.Text = service.Title;
+            txtDescription.Text = service.Description;
+            txtCost.Text = service.Cost.ToString();
+            txtDuration.Text = service.DurationInSeconds.ToString();
+            txtDiscount.Text = service.Discount.ToString();
+            imageService.Source = new BitmapImage(new Uri(service.MainImagePath, UriKind.Relative));
+            btnAddUpdate.Content = "Изменить услугу";
+        }
+
+        private void btnAddUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            if (service == null)
+            {
+                service = new Service();
+                DataBase.connection.Service.Add(service);
+            }
+            service.Title = txtName.Text;
+            service.Cost = Convert.ToDecimal(txtCost.Text);
+            service.DurationInSeconds = Convert.ToInt32(txtDuration.Text);
+            service.Description = txtDescription.Text;
+            service.Discount = Convert.ToDouble(txtDiscount.Text);
+            DataBase.connection.SaveChanges();
+            MessageBox.Show("Услуга успешно добавлена!", "Добавление услуги", MessageBoxButton.OK, MessageBoxImage.Information);
+            this.Close();
         }
     }
 }
--- DemoEkzam/Pages/Vyvod.xaml.cs
+++ DemoEkzam/Pages/Vyvod.xaml.cs
@@ -24,64 +24,54 @@ namespace DemoEkzam.Pages
         public Vyvod()
         {
             InitializeComponent();
-            cbSort.SelectedIndex = 0;
             cbFiltr.SelectedIndex = 0;
         }
-        static List<Service> list = DataBase.connection.Service.ToList();
-        List<Service> list1 = list;
-        int index = 0;
-        int indexAll = list.Count;
 
         private void cbFiltr_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            SearchTitle(list);
-            Filtr(list1);
-            Sort();
+            Filtr();
         }
 
-        void Filtr(List<Service> L)
+        void Filtr()
         {
+            List<Service> list = DataBase.connection.Service.ToList();
             switch (cbFiltr.SelectedIndex)
             {
-                case 0:
-                    list1 = list;
-                    break;
                 case 1:
-                    list1 = L.Where(s => s.Discount >= 0 && s.Discount < 0.05).ToList();
+                    list = list.Where(s => s.Discount >= 0 && s.Discount < 0.05).ToList();
                     break;
                 case 2:
-                    list1 = L.Where(s => s.Discount >= 0.05 && s.Discount < 0.15).ToList();
+                    list = list.Where(s => s.Discount >= 0.05 && s.Discount < 0.15).ToList();
                     break;
                 case 3:
-                    list1 = L.Where(s => s.Discount >= 0.15 && s.Discount < 0.30).ToList();
+                    list = list.Where(s => s.Discount >= 0.15 && s.Discount < 0.30).ToList();
                     break;
                 case 4:
-                    list1 = L.Where(s => s.Discount >= 0.30 && s.Discount < 0.70).ToList();
+                    list = list.Where(s => s.Discount >= 0.30 && s.Discount < 0.70).ToList();
                     break;
                 case 5:
-                    list1 = L.Where(s => s.Discount >= 0.70 && s.Discount < 1).ToList();
+                    list = list.Where(s => s.Discount >= 0.70 && s.Discount < 1).ToList();
                     break;
             }
-        }
-        void Sort()
-        {
-            switch (cbSort.SelectedIndex)
+            if(cbSort.SelectedIndex != -1)
             {
-                case 0:
-                    list1 = list1.OrderBy(s => s.Costnew).ToList();
-                    break;
-                case 1:
-                    list1 = list1.OrderByDescending(s => s.Costnew).ToList();
-                    break;
+                switch (cbSort.SelectedIndex)
+                {
+                    case 0:
+                        list = list.OrderBy(s => s.Cost).ToList();
+                        break;
+                    case 1:
+                        list = list.OrderByDescending(s => s.Cost).ToList();
+                        break;
+                    
+                }
             }
-            lstView.ItemsSource = list1;
-            index = list1.Count;
-            tbCountService.Text = index.ToString() + " из " + indexAll.ToString();
-        }
-
-        void SearchTitle(List<Service> L)
-        {
-            list1 = L.Where(x => x.Title.Contains(txtNameService.Text)||x.Description.Contains(txtNameService.Text)).ToList();
+            if (string.IsNullOrWhiteSpace(txtNameService.Text))
+            {
+                list = list.Where(s => s.Title.Contains(txtNameService.Text)).ToList();
+            }
+            lstView.ItemsSource = list;
+            tbCountService.Text = list.Count.ToString() + " из " + DataBase.connection.Service.ToList().Count.ToString();
         }
 
         bool CheckZapis(int id)
@@ -106,32 +96,24 @@ namespace DemoEkzam.Pages
                 Service service = DataBase.connection.Service.FirstOrDefault(x => x.ID == id);
                 DataBase.connection.Service.Remove(service);
                 DataBase.connection.SaveChanges();
-                list = DataBase.connection.Service.ToList();
-                SearchTitle(list);
-                Filtr(list1);
-                Sort();
                 MessageBox.Show("Запись успешно удалена!", "Удаление записи", MessageBoxButton.OK, MessageBoxImage.Information);
+                FrameLoad.frmLoad.Navigate(new Vyvod());
             }
         }
 
         private void txtNameService_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Filtr(list);
-            SearchTitle(list1);
-            Sort();
+            Filtr();
         }
 
         private void btnUpdate_Click(object sender, RoutedEventArgs e)
         {
-            AddOrUpdateWindow window = new AddOrUpdateWindow();
-            if (window.IsActive)
-            {
-                MessageBox.Show("ervdev");
-            }
-            else
-            {
-                window.Show();
-            }
+            Button btn = (Button)sender;
+            int index = (int)btn.Tag;
+            Service service = DataBase.connection.Service.FirstOrDefault(x => x.ID == index);
+            AddOrUpdateWindow window = new AddOrUpdateWindow(service);
+            window.ShowDialog();
+            FrameLoad.frmLoad.Navigate(new Vyvod());
         }
     }
 }
